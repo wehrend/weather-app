38c9941a785c6d965bd492ce3db17824b46e6b93
Sven Wehrend <swehrend@ant-neuro.com>
2026-05-07 21:00:59 +0200

    implment js logic for favorites handling (task 6)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { loadDetailView } from "./detailView";
+import { renderMainMenu } from "./mainMenu";
 
 export const rootElement = document.getElementById("app");
 
 // await loadDetailView("Berlin");
+
+renderMainMenu();
---
+++ src/mainMenu.js
@@ -0,0 +1,73 @@
+import { loadDetailView } from "./detailView";
+import { rootElement } from "./main";
+
+export function renderMainMenu() {
+  rootElement.innerHTML = `
+    <div class="main-menu">
+    ${getMenuHeaderHtml()}
+    ${getCityListHtml()}
+    </div>
+    `;
+
+  registerEventListeners();
+}
+
+function getMenuHeaderHtml() {
+  return `
+    <div class="main-menu__heading">
+          Weather <button class="main-menu__edit">Edit</button>
+        </div>
+      <div class="main-menu__search-bar">
+        <input
+          type="text"
+          class="main-menu__search-input"
+          placeholder="Search for city..."
+        />
+      </div>
+    `;
+}
+
+function getCityListHtml() {
+  const favoriteCities = ["Oslo", "Stockholm", "Helsinki"];
+
+  const favoriteCitiesElements = [];
+
+  for (let city of favoriteCities) {
+    const cityHtml = `
+        <div class="city-wrapper">
+          <div class="city" data-city-name="${city}">
+            <div class="city__left-column">
+              <h2 class="city__name">${city}</h2>
+              <div class="city__country">Norway</div>
+              <div class="city__condition">sonnig</div>
+            </div>
+            <div class="city__right-column">
+              <div class="city__temperature">20°</div>
+              <div class="city__min-max-temperature">H:21° T:11°</div>
+            </div>
+          </div>
+        </div>
+    `;
+    favoriteCitiesElements.push(cityHtml);
+  }
+
+  const favoriteCitiesHtml = favoriteCitiesElements.join("");
+
+  return `
+        <div class="main-menu__cities-list">
+            ${favoriteCitiesHtml}
+        </div>
+    `;
+}
+
+function registerEventListeners() {
+  const cities = document.querySelectorAll(".city");
+
+  cities.forEach((city) => {
+    city.addEventListener("click", () => {
+      const cityName = city.getAttribute("data-city-name");
+
+      loadDetailView(cityName);
+    });
+  });
+}
