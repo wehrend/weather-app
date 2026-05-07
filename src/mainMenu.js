3a8478b9cb8751d3e4f536d79c0b87e7941c8989
Sven Wehrend <swehrend@ant-neuro.com>
2026-05-07 21:39:07 +0200

    implement fetching correct data (task 6)

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -1,18 +1,21 @@
+import { getForecastWeather } from "./api";
 import { loadDetailView } from "./detailView";
 import { renderLoadingScreen } from "./loading";
 import { rootElement } from "./main";
+import { getConditionImagePath } from "./conditions";
+import { formatTemperature } from "./utils";
 
-export function loadMainMenu() {
+export async function loadMainMenu() {
   rootElement.classList.remove("show-background");
   renderLoadingScreen("Load overview...");
-  renderMainMenu();
+  await renderMainMenu();
 }
 
-export function renderMainMenu() {
+export async function renderMainMenu() {
   rootElement.innerHTML = `
     <div class="main-menu">
     ${getMenuHeaderHtml()}
-    ${getCityListHtml()}
+    ${await getCityListHtml()}
     </div>
     `;
 
@@ -34,23 +37,34 @@ function getMenuHeaderHtml() {
     `;
 }
 
-function getCityListHtml() {
-  const favoriteCities = ["Oslo", "Stockholm", "Helsinki"];
+async function getCityListHtml() {
+  const favoriteCities = ["Oslo", "Rio de Janeiro", "Helsinki"];
 
   const favoriteCitiesElements = [];
 
   for (let city of favoriteCities) {
+    const weatherData = await getForecastWeather(city, 1);
+    const { location, current, forecast } = weatherData;
+    const currentDay = forecast.forecastday[0];
+
+    const conditionImage = getConditionImagePath(
+      current.condition.code,
+      current.is_day !== 1,
+    );
     const cityHtml = `
         <div class="city-wrapper">
-          <div class="city" data-city-name="${city}">
+          <div class="city" 
+          style="--condition-image: url(${conditionImage})" 
+          data-city-name="${city}">
             <div class="city__left-column">
-              <h2 class="city__name">${city}</h2>
-              <div class="city__country">Norway</div>
-              <div class="city__condition">sonnig</div>
+              <h2 class="city__name">${location.name}</h2>
+              <div class="city__country">${location.country}</div>
+              <div class="city__condition">${current.condition.text}</div>
             </div>
             <div class="city__right-column">
-              <div class="city__temperature">20°</div>
-              <div class="city__min-max-temperature">H:21° T:11°</div>
+              <div class="city__temperature">${formatTemperature(current.temp_c)}°</div>
+              <div class="city__min-max-temperature">H:${formatTemperature(currentDay.day.maxtemp_c)}° T:
+              ${formatTemperature(currentDay.day.mintemp_c)}°</div>
             </div>
           </div>
         </div>
